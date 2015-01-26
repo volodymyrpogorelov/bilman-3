392b8586c70461b3bd6d0e43e157dab10bdac964
Volodymyr.Pogorelov@gmail.com <Volodymyr.Pogorelov@gmail.com>
2015-01-26 16:54:39 +0200

    Other pages have been added to the project.

--- app/controllers/Application.scala
+++ app/controllers/Application.scala
@@ -8,5 +8,29 @@ object Application extends Controller {
   def index = Action {
     Ok(views.html.index("Your new application is ready."))
   }
+  
+  def news = Action {
+    Ok(views.html.news("Your new application is ready."))
+  }
+  
+  def schedule = Action {
+    Ok(views.html.schedule("Your new application is ready."))
+  }
+  
+  def photos = Action {
+    Ok(views.html.photos("Your new application is ready."))
+  }
+  
+  def videos = Action {
+    Ok(views.html.videos("Your new application is ready."))
+  }
+  
+  def contacts = Action {
+    Ok(views.html.contacts("Your new application is ready."))
+  }
+  
+  def forum = Action {
+    Ok(views.html.forum("Your new application is ready."))
+  }
 
 }
