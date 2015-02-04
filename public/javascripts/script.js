190da9e6074bfc1fb9499c81f26375bc18f4a847
Volodymyr.Pogorelov@gmail.com <Volodymyr.Pogorelov@gmail.com>
2015-02-04 16:09:51 +0200

    Dynamic menu update

--- public/javascripts/script.js
+++ public/javascripts/script.js
@@ -1,13 +1,16 @@
 
-var direction = 1;
-var DEFAULT_MARGIN_TOP = '4%';
-var DISTANCE = 20;
-function movemenu (currentItemId, nextItemId) {
-	var el = document.getElementById(nextItemId);
-	if(direction > 0){
-		el.style.marginTop = ' ' + 2*DISTANCE + '%';
-	}else{
-		el.style.marginTop = DEFAULT_MARGIN_TOP;
+var subMenuIds = {'menu_item_container03' : ['sub_menu_item_container03_01', 'sub_menu_item_container03_02']}
+
+function movemenu (currentItemId) {
+	var ids = subMenuIds[currentItemId];
+	for (i = 0; i < ids.length; i++){
+		var el = document.getElementById(ids[i]);
+		if(el.style.visibility === 'visible'){
+			el.style.position = 'absolute';
+			el.style.visibility = 'hidden';
+		}else {
+			el.style.position = 'relative';
+			el.style.visibility = 'visible';
+		}
 	}
-	direction*=-1;
-}
+} 
