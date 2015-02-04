6358940480dcd7f0f9239ee581350cd162c9ce05
vladimir <Volodymyr.Pogorelov@gmail.com>
2015-02-04 11:06:01 +0200

    Menu is more dynamic

--- public/javascripts/script.js
+++ public/javascripts/script.js
@@ -1,3 +1,13 @@
-function movemenu (id, distance) {
-	alert("It works!!!");
+
+var direction = 1;
+var DEFAULT_MARGIN_TOP = '4%';
+var DISTANCE = 20;
+function movemenu (currentItemId, nextItemId) {
+	var el = document.getElementById(nextItemId);
+	if(direction > 0){
+		el.style.marginTop = ' ' + 2*DISTANCE + '%';
+	}else{
+		el.style.marginTop = DEFAULT_MARGIN_TOP;
+	}
+	direction*=-1;
 }
