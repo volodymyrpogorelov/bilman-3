8969a654bdade5f85fb7910573700b11b6a3a7a5
vladimir <Volodymyr.Pogorelov@gmail.com>
2015-03-06 00:40:44 +0200

    Calendar updated

--- app/controllers/utils/CalendarUtils.scala
+++ app/controllers/utils/CalendarUtils.scala
@@ -4,18 +4,34 @@ import java.util.Calendar
 
 object CalendarUtils {
   private val instance = Calendar.getInstance();
+  private val months = Map(0 -> "Январь",
+                        1 -> "Февраль",
+                        2 -> "Март",
+                        3 -> "Апрель",
+                        4 -> "Май",
+                        5 -> "Июнь",
+                        6 -> "Июль",
+                        7 -> "Август",
+                        8 -> "Сентябрь",
+                        9 -> "Октябрь",
+                        10 -> "Ноябрь",
+                        11 -> "Декабрь")
   
   val getYear : String = instance.get(Calendar.YEAR).toString()
-  val getMonth : String = instance.get(Calendar.MONTH).toString()
+  val getMonth : String = months.get(instance.get(Calendar.MONTH)) match {
+    case Some (res) => res
+    case None => ""
+  }
   
   private val daysInMonth = instance.getActualMaximum(Calendar.DAY_OF_MONTH)
   private val dayOfWeekForBegOfMonth = {
     val tmpCalendar = Calendar.getInstance()
-    tmpCalendar.set(Calendar.YEAR, Calendar.MONTH, 1)
-    tmpCalendar.get(Calendar.DAY_OF_WEEK)
+    tmpCalendar.set(instance.get(Calendar.YEAR), instance.get(Calendar.MONTH) - 1, 1)
+    val tmp = tmpCalendar.get(Calendar.DAY_OF_WEEK_IN_MONTH)
+    tmp
   }
   def getDays(rw : Int, cl : Int) : Int = {
-    val res = rw*7 + cl + 2 - dayOfWeekForBegOfMonth;
+    val res = rw*7 + cl + 1 - dayOfWeekForBegOfMonth;
     if((res < 0) || (res > daysInMonth)){
       0
     }else{
