7d0406d4a050c0c3433c82829d3d519fbe029af0
Volodymyr.Pogorelov@gmail.com <Volodymyr.Pogorelov@gmail.com>
2015-03-06 18:01:16 +0200

    Calendar is updated.

--- app/controllers/utils/CalendarUtils.scala
+++ app/controllers/utils/CalendarUtils.scala
@@ -4,18 +4,28 @@ import java.util.Calendar
 
 object CalendarUtils {
   private val instance = Calendar.getInstance();
-  private val months = Map(0 -> "Январь",
-                        1 -> "Февраль",
-                        2 -> "Март",
-                        3 -> "Апрель",
-                        4 -> "Май",
-                        5 -> "Июнь",
-                        6 -> "Июль",
-                        7 -> "Август",
-                        8 -> "Сентябрь",
-                        9 -> "Октябрь",
-                        10 -> "Ноябрь",
-                        11 -> "Декабрь")
+  private val months = Map(
+                        Calendar.JANUARY -> "Январь",
+                        Calendar.FEBRUARY -> "Февраль",
+                        Calendar.MARCH -> "Март",
+                        Calendar.APRIL -> "Апрель",
+                        Calendar.MAY -> "Май",
+                        Calendar.JUNE -> "Июнь",
+                        Calendar.JULY -> "Июль",
+                        Calendar.AUGUST -> "Август",
+                        Calendar.SEPTEMBER -> "Сентябрь",
+                        Calendar.OCTOBER -> "Октябрь",
+                        Calendar.NOVEMBER -> "Ноябрь",
+                        Calendar.DECEMBER -> "Декабрь")
+                        
+  private val weekDaysBckw = Map(
+                        Calendar.MONDAY -> 0,
+                        Calendar.TUESDAY -> 1,
+                        Calendar.WEDNESDAY -> 2,
+                        Calendar.THURSDAY -> 3,
+                        Calendar.FRIDAY -> 4,
+                        Calendar.SATURDAY -> 5,
+                        Calendar.SUNDAY -> 6)
   
   val getYear : String = instance.get(Calendar.YEAR).toString()
   val getMonth : String = months.get(instance.get(Calendar.MONTH)) match {
@@ -26,17 +36,23 @@ object CalendarUtils {
   private val daysInMonth = instance.getActualMaximum(Calendar.DAY_OF_MONTH)
   private val dayOfWeekForBegOfMonth = {
     val tmpCalendar = Calendar.getInstance()
-    tmpCalendar.set(instance.get(Calendar.YEAR), instance.get(Calendar.MONTH) - 1, 1)
-    val tmp = tmpCalendar.get(Calendar.DAY_OF_WEEK_IN_MONTH)
-    tmp
+    tmpCalendar.set(instance.get(Calendar.YEAR), instance.get(Calendar.MONTH), 1)
+    weekDaysBckw.get(tmpCalendar.get(Calendar.DAY_OF_WEEK)) match {
+      case Some (res) => res
+      case None => 0
+    }
   }
   def getDays(rw : Int, cl : Int) : Int = {
     val res = rw*7 + cl + 1 - dayOfWeekForBegOfMonth;
     if((res < 0) || (res > daysInMonth)){
       0
     }else{
-      res
+      if(res == getCurrentDay){
+        -res
+      }else {
+        res
+      }
     }
   }
-  val getCurrentDay : Integer = instance.get(Calendar.DAY_OF_MONTH)
+  val getCurrentDay : Integer = 29//instance.get(Calendar.DAY_OF_MONTH)
 }
