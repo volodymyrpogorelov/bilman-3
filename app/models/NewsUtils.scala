69395c736f94a3150ac80ef1862481da36209530
vladimir <Volodymyr.Pogorelov@gmail.com>
2015-03-16 00:01:51 +0200

    News up

--- app/models/NewsUtils.scala
+++ app/models/NewsUtils.scala
@@ -6,27 +6,16 @@ import scala.collection.mutable.ListBuffer
 object NewsUtils {
   private val allNews = {
     var res : List[NewsDescriptor] = List()
-    //val source = scala.io.Source.fromFile("/home/vladimir/workspace/bilman-3/app/models/news_db.txt")
-    val source = scala.io.Source.fromFile("/home/pogorelov/Tmp/tmp/workspace/bilman-3/app/models/news_db.txt")
-    val lines = source.getLines()
+    val lines = 1 to 100
     
     val acc : StringBuilder = new StringBuilder("")
     var state = 0
     var data = ""
     var index = 0
     lines.foreach { line => 
-      if(line.trim() == "BEG"){
-        acc.clear()
-        state = 1
-      }else if(state == 1){
-        data = line.trim()
-        state = 0
-      }else if (line.trim() == "END"){
         res = (new NewsDescriptor(s"$index) 2015-01-01", "С новым 2015 годом!!!")) :: res 
         index += 1
-      }
     }
-    source.close()
     res.reverse
   }
   
