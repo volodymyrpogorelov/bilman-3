13bbb117d71e6f463e535917499ceaebb7192683
Volodymyr.Pogorelov@gmail.com <Volodymyr.Pogorelov@gmail.com>
2015-09-28 16:25:50 +0300

    Update

--- app/models/NewsUtils.scala
+++ app/models/NewsUtils.scala
@@ -2,7 +2,8 @@ package models
 
 import scala.collection.mutable.ListBuffer
 import java.util.GregorianCalendar
-import java.util
+import java.text.SimpleDateFormat
+
 
 
 object NewsUtils {
@@ -14,6 +15,10 @@ object NewsUtils {
     var state = 0
     var data = ""
     var index = 0
+    
+    val date_s : String  = " 2011-01-18 00:00:00.0"; 
+    val dt : SimpleDateFormat = new SimpleDateFormat("yyyy-mm-dd hh:mm:ss"); 
+    val date = dt.parse(date_s); 
     lines.foreach { line => 
         res = (new NewsDescriptor(new GregorianCalendar(2013,10,28), "Привітання", "С новым 2015 годом!!!")) :: res 
         index += 1
