af83ed7dcd74d5cf023eab08ce333cbf70b1e436
volodymyrpogorelov <volodymyr.pogorelov@gmail.com>
2015-10-18 18:14:34 +0300

    Fix for photo_gallery and club name.

--- app/controllers/utils/PhotoGalleryUtils.scala
+++ app/controllers/utils/PhotoGalleryUtils.scala
@@ -4,20 +4,23 @@ package controllers.utils
  * @author pogorelov
  */
 
-import java.io.File
-
 object PhotoGalleryUtils {
   
-  private def getListOfFiles(dir: String) : List[File] = {
-    val d = new File(dir)
-    if (d.exists && d.isDirectory) {
-      d.listFiles.filter(_.isFile).toList
-    } else {
-      List[File]()
-    }
-  }
+  private val getListOfFiles : List[String] = List[String](
+    "38_UTSyCQuw.jpg",
+    "45cGXJqP5Lo.jpg",
+    "4nJHVXcTfCk.jpg",
+    "JMSgYM6g5u8.jpg",
+    "jwkqOanMtGA.jpg",
+    "Q5C1CkI4rxk.jpg",
+    "qDtjYCCJ4vk.jpg",
+    "rSiEnmKzEHU.jpg",
+    "s373rZ4OGQ8.jpg",
+    "TLtnEptcxz0.jpg"
+  )
+    
   
-  val allPhotosPath = getListOfFiles("./public/photos/gallery/").map(f => "assets/photos/gallery/" + f.getName())
-  val currentPath = new java.io.File( "." ).getCanonicalPath
+  
+  val allPhotosPath = getListOfFiles.map(f => "assets/photos/gallery/" + f)
   
 }
